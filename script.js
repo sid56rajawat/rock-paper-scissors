d9fc47f8fb1848e6f512c1bca94df707c5790ccf
sid56rajawat <sid56rajawat@gmail.com>
2023-07-07 19:00:34 +0530

    add simple UI to rock-paper-scissors
    May be prone to some bugs but basic game is working

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const CHOICES = ['rock','paper','scissors'];
 const RULES = ['rock beats scissors','paper beats rock','scissors beats paper'];
+let roundsPlayed = 0,playerWins = 0, computerWins = 0, totalRounds;
+
+const choicesDiv = document.querySelector(".choices");
+const choiceButtons = document.querySelectorAll(".choice");
+choiceButtons.forEach(button => button.addEventListener('click',() => playRound(button.innerText.toLowerCase(),getComputerChoice())));
+
+const output = document.querySelector(".output");
+
+const submitButton = document.querySelector("button[type='submit']");
 
 function getComputerChoice(){
     let i = Math.floor(Math.random() * 3);
@@ -7,57 +16,46 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection,computerSelection){
-    if(playerSelection === computerSelection) return [`Nobody Wins! ${playerSelection} does not beat ${computerSelection}`,0];
-    ans =["",0];
+    let ans;
+    roundsPlayed++;
+
+    if(playerSelection === computerSelection){
+        ans = `Nobody Wins! ${playerSelection} does not beat ${computerSelection}`;
+    }
+    
     for(let rule of RULES){
         let playerIndex = rule.search(playerSelection);
         let computerIndex = rule.search(computerSelection);
 
-        if(playerIndex !== -1 && computerIndex !== -1){
+        if(playerIndex !== -1 && computerIndex !== -1 && playerSelection !== computerSelection){
             if(playerIndex < computerIndex){
-                ans[0] = "You Win! ";
-                ans[1] = 1;
+                ans = "You Win! ";
+                playerWins++;
             }
             else{
-                ans[0] = "You Lose! ";
-                ans[1] = -1;
+                ans = "You Lose! ";
+                computerWins++;
             }
-            ans[0] += rule;
+            ans += rule;
         }
     }
+
     
-    return ans;
-}
+    output.innerText += "\n" + ans;
 
-function isValidChoice(choice){
-    return CHOICES.findIndex(item => item === choice) === -1 ? false : true;
-}
-
-function game(){
-    const ROUNDS = prompt("Enter number of rounds : ",5);
-    let counter = ROUNDS, playerWins = 0, computerWins = 0;
-    while(counter--){
-        if(playerWins > (ROUNDS / 2) || computerWins > (ROUNDS / 2)) break;
-
-        console.log(`round ${ROUNDS - counter}`);
-
-        let playerSelection = prompt("Enter your choice (rock/paper/scissors) : ");
-        if(!playerSelection) break;
-        playerSelection = playerSelection.toLowerCase();
-        if(!isValidChoice(playerSelection)){
-            counter++;
-            continue;
-        }
-        
-        let computerSelection = getComputerChoice();
-
-        let ans = playRound(playerSelection,computerSelection);
-        console.log(ans[0]);
-
-        if(ans[1] > 0) playerWins++;
-        else if(ans[1] < 0) computerWins++;
+    if(roundsPlayed >= totalRounds || playerWins > (totalRounds / 2) || computerWins > (totalRounds / 2)){
+        choicesDiv.style.display = "none";
+        let winner = playerWins === computerWins ? "Nobody" : playerWins > computerWins ? "Player" : "Computer";
+        output.innerText += `\nThe final winner is ${winner}!`;
     }
-    let winner = playerWins === computerWins ? "Nobody" : playerWins > computerWins ? "Player" : "Computer";
-    console.log("The final winner is " + winner);
+
 }
 
+submitButton.addEventListener('click',() => {
+    totalRounds = Number(document.querySelector('#rounds').value);
+    choicesDiv.style.display = "block";
+    output.innerText = "";
+    roundsPlayed = 0;
+    playerWins = 0;
+    computerWins = 0;
+});
